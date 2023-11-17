550f39e3437a05a97a7276fa668afee999f50a17
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-11-17 11:56:06 -0600

    added working fetch to api for sign up

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -9,10 +9,28 @@ const SignUpForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         console.log("submitted")
+        try{
+            const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', 
+            { 
+              method: "POST", 
+              headers: { 
+                "Content-Type": "application/json" 
+              }, 
+              body: JSON.stringify({ 
+                username: username, 
+                password: password 
+              }) 
+            })
+            const result = await response.json()
+            console.log(result)
+        }catch(error){setError(error.message)}
     }
     return <>
             <h2>SignUp</h2>
-
+            {
+                error? <p>{error}</p> :
+                <p>Everything ok</p>
+            }
             <form onSubmit={handleSubmit}>
                 <label>Username
                     <input value={username} onChange={(e) => {
